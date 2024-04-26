8d4b20d1498013bf33b05f27f95cd91ae4f31090
shyamp109 <shyamp.itpath@gmail.com>
2024-04-26 14:27:33 +0530

    camera design complete

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,6 @@
 export const IMAGES = {
   splashCar: require('./images/logo.png'),
+  camera: require('./images/camera.png'),
+  close: require('./images/close.png'),
+  gallery: require('./images/image.png'),
 };
--- src/screens/RecordVideo/index.jsx
+++ src/screens/RecordVideo/index.jsx
@@ -1,10 +1,20 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import React, {useEffect} from 'react';
 import {
   Camera,
   useCameraDevice,
   useCameraPermission,
 } from 'react-native-vision-camera';
+import {IMAGES} from '../../assets';
+import {height, width} from '../../utills/helper';
+
 const RecordVideo = () => {
   const device = useCameraDevice('back');
   const {hasPermission} = useCameraPermission();
@@ -19,17 +29,80 @@ const RecordVideo = () => {
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
-      <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+      <View style={styles.container}>
+        <Text style={styles.text}>Take a Video</Text>
+        <Camera style={styles.camera} device={device} isActive={true} />
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Image
+              source={IMAGES.close}
+              style={{
+                height: height * 0.08,
+                width: height * 0.08,
+                resizeMode: 'contain',
+                tintColor: 'white',
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Image
+              source={IMAGES.camera}
+              style={{
+                height: height * 0.08,
+                width: height * 0.08,
+                resizeMode: 'contain',
+                tintColor: 'white',
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Image
+              source={IMAGES.gallery}
+              style={{
+                height: height * 0.08,
+                width: height * 0.08,
+                resizeMode: 'contain',
+                tintColor: 'white',
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
-export default RecordVideo;
-
 const styles = StyleSheet.create({
   safeAreaView: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  text: {
+    fontSize: height * 0.025,
+    color: 'white',
+    marginBottom: height * 0.02,
+  },
+  camera: {
+    width: '100%',
+    height: height * 0.6,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: 10,
+    marginTop: height * 0.02,
+    width: width * 0.98,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
+
+export default RecordVideo;
